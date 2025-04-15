aeb4ce58cafcb436fb2c87704f7bf7ed8e6dd613
Cantonplas <dcancat@etsinf.upv.es>
2025-04-15 19:59:39 +0200

    Added all of david's work

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "***************"   // DateTime using ISO-8601 format
+        "20250415T195755"   // DateTime using ISO-8601 format
         " "                 // alignment
-        "--------"          // STLIB commit
+        "ce53a8ad"          // STLIB commit
         "--------"          // ADJ commit
-        "--------"          // Board commit
+        "948cd11b"          // Board commit
         // the '=' is used for unparsing
         ;
 }
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -5,20 +5,65 @@
 #endif
 
 #include "ST-LIB.hpp"
+#include "BLCU/BLCU.hpp"
+
+
+HeapOrder ack = {
+    3,
+};
+
+
+HeapOrder nack = {
+    4,
+};
+
+HeapOrder get_version_order = {
+    703,
+    BLCU::get_version,
+    &BLCU::orders_data.target,
+    &BLCU::orders_data.version,
+};
 
 int main(void) {
 #ifdef SIM_ON
     SharedMemory::start();
 #endif
 
-    DigitalOutput led_on(PB1);
-    STLIB::start();
+    HeapOrder write_program_order = {
+        700,
+        BLCU::write_program,
+        &BLCU::orders_data.target,
+    };
 
-    Time::register_low_precision_alarm(5, [&]() { led_on.toggle(); 
-    });
+    HeapOrder read_program_order = {
+        701,
+        BLCU::read_program,
+        &BLCU::orders_data.target,
+    };
+
+    HeapOrder erase_program_order = {
+        702,
+        BLCU::erase_program,
+        &BLCU::orders_data.target,
+    };
+
+    HeapOrder reset_all_order = {
+        704,
+        BLCU::reset_all,
+        &BLCU::orders_data.target,
+    };
+
+    BLCU::set_up();
+    BLCU::start();
+    printf("\n\n\n\n\n\n");
+    printf("BLCU fully initiated!\n");
+
+    BLCU::orders_data.target = BLCU::Target::PCU;
+    BLCU::read_program();
+
 
     while (1) {
-        STLIB::update();
+    BLCU::update();
     }
 }
 
