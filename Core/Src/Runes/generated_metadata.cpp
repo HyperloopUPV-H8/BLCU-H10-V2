a9f7024e22729f685790954b81b349bfa17ed72b
Cantonplas <dcancat@etsinf.upv.es>
2025-05-06 21:37:57 +0200

    Deps changes

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250505T193101"   // DateTime using ISO-8601 format
+        "20250505T200810"   // DateTime using ISO-8601 format
         " "                 // alignment
         "f6c269b9"          // STLIB commit
         "--------"          // ADJ commit
-        "33f3e3c9"          // Board commit
+        "78b90b31"          // Board commit
         // the '=' is used for unparsing
         ;
 }
